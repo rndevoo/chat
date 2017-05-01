d9bf7dc44d1f4c83df8469edfc10508b11b3c190
Diego Stratta <strattadb@gmail.com>
2017-05-01 19:00:09 -0300

    Add configuration files

---
+++ src/config/winston.js
@@ -0,0 +1,19 @@
+/**
+ * @overview Configure Winston (the logger).
+ */
+'use strict';
+
+import winston from 'winston';
+
+const LOG_LEVEL = process.env.LOG_LEVEL;
+
+winston.configure({
+  transports: [
+    new winston.transports.Console({
+      colorize: true,
+      level: LOG_LEVEL,
+    }),
+  ],
+});
+
+export default winston;
