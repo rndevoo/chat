ff0f4caefdc9d2261dcf80970dbd6a5d0ac922bf
Diego Stratta <strattadb@gmail.com>
2017-05-15 15:41:43 -0300

    Update connection handler

--- src/handlers/connection.js
+++ src/handlers/connection.js
@@ -6,9 +6,10 @@
  */
 'use strict';
 
+import logger from '../config/winston';
+
 import {
   validateUser,
-  getPreviousMessages,
   getAllowedUsers,
 } from './../utils/onConnection';
 
@@ -27,22 +28,47 @@ import {
  *
  * @param {Object} ws - The WebSocket server object.
  */
-export async function connectionHandler (ws: Object) {
+export async function connectionHandler (
+  ws: Object,
+  clients: Map<string, Object>,
+) {
   let user: Object;
   try {
-    user = await validateUser(ws);
+    user = await validateUser(ws, clients);
   } catch (e) {
     /**
      * @todo communicate with the logging service to log this incident.
      */
+
+    // Close the WebSocket connection. There's nothing to do anymore.
+    return ws.close(401, 'Authentication Failed');
   }
-  const allowedUsers: Set<string> = await getAllowedUsers(userId);
+
+  // Store the user WebSocket connection.
+  clients.set(user.id, ws);
+
+  const allowedUsers: Set<string> = await getAllowedUsers(user.id);
 
   ws.on('message', async (message) => {
     if (message.type === 'message') {
+      const recipientId: string = message.to;
       // Verify that the sender is allowed to send a message to this user.
-      if (!await verifyRecipient(message.to, allowedUsers)) {
+      if (await verifyRecipient(recipientId, allowedUsers)) {
+        // Try to get the WebSocket connection of the recipient.
+        const recipientConnection = clients.get(recipientId);
 
+        // If the recipient is connected send the message through the socket.
+        if (recipientConnection) {
+          await sendMessage(message.data, recipientConnection);
+        } else {
+          /**
+           * @todo Store somewhere the messages to deliver them later
+           * when the recipient is online.
+           */
+        }
+      } else {
+        ws.send({ type: 'error', error: 'Recipient not allowed.' });
+        logger.warn('User attempted unauthorized communication.');
       }
     }
   });
